23684730da9e871664dc7b78246dd64e8c5fb1b5
Wada Erna <wada@openmailbox.org>
2015-04-26 23:00:21 -0600

    Script blocking advertising via hosts file

---
+++ adBlock.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#: Title	: adBlock.sh
+#: Date		: 17/03/15
+#: Description	: Modific file hosts and add files.
+#: Version	: 1.0
+#: Autor	: Wada Erna
+
+# Start time elapsed
+startTime=$(date +%s)
+
+# URLS & Temp Files
+declare -a myUrls=( 
+    http://winhelp2002.mvps.org/hosts.txt 
+    http://www.malwaredomainlist.com/hostslist/hosts.txt )
+declare -a tmpFiles=()
+
+# Store directories
+adHosts="/etc/hosts"
+adHstBK="/etc/hosts.bk"
+tmpTrash="/tmp/trash"
+tmpLineOut="/tmp/lineout"
+
+fncDownload() {
+    printf "::Start DB update \n"
+    for((i=0; i<=$((${#myUrls[@]}-1));i++)); do
+	rute="/tmp/FO$i.txt"
+	curl -o $rute -O ${myUrls[$i]} &> /dev/null
+	tmpFiles+=("$rute")
+    done
+    printf "::Download all DB\n"
+}
+
+fncHostBackup() {
+    if [[ -f /etc/hosts.bk ]]; then
+	printf "!!There is a copy of hosts, skiping a backup\n"
+    else
+	printf "::Making a copy of hosts\n"
+	sudo bash -c "cp $adHosts $adHstBK"
+    fi
+}
+
+fncClean(){
+    for((i=0; i<=$((${#tmpFiles[@]}-1));i++)); do
+	egrep -v "localhost" ${tmpFiles[$i]} > $tmpLineOut
+	cat $tmpLineOut | col -b >> $tmpTrash
+    done
+    printf "::Cleaned all files\n"
+}
+
+fncReplaceHost(){
+    sudo bash -c "cat $adHstBK $tmpTrash > $adHosts"
+    printf "::Replace host complete\n"
+}
+
+fncDelete(){
+    sudo bash -c "rm $tmpTrash $tmpLineOut"
+    for((i=0; i<=$((${#tmpFiles[@]}-1));i++)); do
+	sudo bash -c "rm ${tmpFiles[$i]}"
+    done
+    unset myUrls
+    unset tmpFiles
+    printf "::Unset and delete all files\n"
+}
+
+fncMain(){
+    fncDownload 
+    fncHostBackup
+    fncClean
+    fncReplaceHost
+    fncDelete
+}
+
+fncMain
+
+# End time elapsed and print it
+endTime=$(date +%s)
+printf "Runing Time: %d\n" $(($endTime-$startTime))
+
+return 0
